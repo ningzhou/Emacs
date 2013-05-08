5dbbaaf6c64732587c65aaf1fae63ee1cfb8b008
ning zhou <ning@t5500.(none)>
2013-05-08 15:31:49 -0400

    auto

--- programming-conf.el
+++ programming-conf.el
@@ -1,20 +1,23 @@
 ;; -----------------------define the "main" skeleton------------------------------
-(define-skeleton skeleton-c-mode-main-func
-  "generate int main(int argc, char * argv[]) automatic" nil
-  "int main (int argc, char * argv[]) \n{\n"
-  > _  "\n" > "return 0;"
-  "\n}")
+;; (define-skeleton skeleton-c-mode-main-func
+;;   "generate int main(int argc, char * argv[]) automatic" nil
+;;   "int main (int argc, char * argv[]) \n{\n"
+;;   > _  "\n" > "return 0;"
+;;   "\n}")
 
-(define-abbrev-table 'c-mode-abbrev-table '(
-    ("main" "" skeleton-c-mode-main-func 1)))
+;; (define-abbrev-table 'c-mode-abbrev-table '(
+;;     ("main" "" skeleton-c-mode-main-func 1)))
 
-(define-abbrev-table 'c-mode-abbrev-table '(
-     ("main" "" skeleton-c-mode-main-func 1))
-)
+;; (define-abbrev-table 'c-mode-abbrev-table '(
+;;      ("main" "" skeleton-c-mode-main-func 1))
+;; )
 
-(define-abbrev-table 'c++-mode-abbrev-table '(
-     ("main" "" skeleton-c-mode-main-func 1))
-)
+;; (define-abbrev-table 'c++-mode-abbrev-table '(
+;;      ("main" "" skeleton-c-mode-main-func 1))
+;; )
+
+
+
 ;; ---------------------auto pair-----------------------------
 (defun my-auto-pair ()
   (interactive)
@@ -44,8 +47,13 @@
   ;; (add-hook hook 'subword-mode) ;; TODO
   ;; (add-hook hook 'enable-which-function)
   )
+;;--------------------c-mode configuration--------------------
+(require 'cc-mode)
+(setq-default c-basic-offset 4 c-default-style "linux")
+(setq-default tab-width 4 indent-tabs-mode t)
+(define-key c-mode-base-map (kbd "RET") 'newline-and-indent)
 
-;;------------------switch between .h <--> .cpp/.m/.cxx
+;;------------------switch between .h <--> .cpp/.m/.cxx--------------------
 (defun switch-head2source-file ()
   "if current file is a header file, then open the corresponding source file or vice versa."
   (interactive)
@@ -77,27 +85,35 @@
 (global-set-key [(control c) (control t)] 'switch-head2source-file)
 
 ;;-------------get etags file intelligently-----------------
-;;(add-to-list 'load-path (concat EMACS_VENDOR "etags-extend/"))
-;;(require 'etags-select)
-;;(require 'etags-table)
-;;(setq etags-table-search-up-depth 10)
-;; --8<-------------------------- separator ------------------------>8--
-;; (defun create-tags (directory)
-;;   "Create tags file."
-;;   (interactive "DDirectory: ")
-;;   (let ((code-file-extension-list '("*.py" "*.sh" "*.rb" "*.pl"
-;;                                     "*.h" "*.cpp" "*.c" "*.cxx"
-;;                                     "*.erl" "*.java" "*.el")))
-;;     (dolist (code-file code-file-extension-list)
-;;       (shell-command (format "find %s -name \"%s\" | etags -a -"
-;;                              directory code-file)))))
+(add-to-list 'load-path (concat EMACS_VENDOR "/etags-extend/"))
+(require 'etags-select)
+(require 'etags-table)
+(setq etags-table-search-up-depth 10)
+;; create tags for the files in a directory
+(defun create-tags (directory)
+  "Create tags file."
+  (interactive "DDirectory: ")
+  (let ((code-file-extension-list '("*.py" "*.sh" "*.rb" "*.pl"
+                                    "*.h" "*.cpp" "*.c" "*.cxx"
+                                    "*.erl" "*.java" "*.el")))
+    (dolist (code-file code-file-extension-list)
+      (shell-command (format "find %s -name \"%s\" | etags -a -"
+                             directory code-file)))))
 
-
-;; python ide provided by Gabriele Lanaro 
+;;--------------------python ide provided by Gabriele Lanaro --------------------
 ;;;1. install the dependencies
 ;;; $ sudo apt-get install pymacs pyflakes
 ;;;2. get the package emacs-for-python and put somewhere
-(load-file (concat EMACS_DIR "/vendor/emacs-for-python/epy-init.el"))
+;;; side effects: yasnippet, eshell and auto-complete configurations
+(add-to-list 'load-path (concat EMACS_VENDOR "/emacs-for-python/") ;; tell where to load the various files
+(require 'epy-setup)      ;; It will setup other loads, it is required!
+(require 'epy-python)     ;; If you want the python facilities [optional]
+(require 'epy-completion) ;; If you want the autocompletion settings [optional]
+(require 'epy-editing)    ;; For configurations related to editing [optional]
+(require 'epy-bindings)   ;; For my suggested keybindings [optional]
+(require 'epy-nose)       ;; For nose integration
+
+
 ;;(setq skeleton-pair nil)
 ;; Python Hook
 ;; (add-hook 'python-mode-hook
@@ -128,9 +144,77 @@
 ;;       `((".*" ,temporary-file-directory t))
 
 
+;;--------------------define smart compile behavior --------------------
+(global-set-key (kbd "<f8>") 'smart-compile)
+(defun smart-compile()
+  "If makefile exists, compile with it. Otherwise build compile instructions"
+  (interactive)
+  (let ((candidate-make-file-name '("makefile" "Makefile" "GNUmakefile"))
+        (command nil))
+    (if (not (null
+              (find t candidate-make-file-name :key
+                    #'(lambda (f) (file-readable-p f)))))
+        (setq command "make -k ")
+      ;; Makefile doesn't exist
+      (if (null (buffer-file-name (current-buffer)))
+          (message "Buffer not attached to a file, won't compile!")
+        (if (eq major-mode 'c-mode)
+            (setq command
+                  (concat "gcc -Wall -o "
+                          (file-name-sans-extension
+                           (file-name-nondirectory buffer-file-name))
+                          " "
+                          (file-name-nondirectory buffer-file-name)
+                          " -g -lm "))
+          (if (eq major-mode 'c++-mode)
+              (setq command
+                    (concat "g++ -Wall -o "
+                            (file-name-sans-extension
+                             (file-name-nondirectory buffer-file-name))
+                            " "
+                            (file-name-nondirectory buffer-file-name)
+                            " -g -lm "))
+            (message "Unknow mode, won't compile!")))))
+    (if (not (null command))
+        (let ((command (read-from-minibuffer "Compile command: " command)))
+          (compile command)))))
+
 ;;------------matlab-emacs mode--------------------
 ;;To enable emacsclient in your emacs
 (server-start)
 ;;setup the matlab-emacs mode
 (add-to-list 'load-path (concat EMACS_VENDOR "/matlab-emacs/matlab-emacs/"))
 (load-library "matlab-load")
+
+;;--------------------gdb configuration--------------------
+(setq gdb-many-windows t)
+(defun show-debugger ()
+  (interactive)
+  (let ((gud-buf
+         (catch 'found
+           (dolist (buf (buffer-list))
+             (if (string-match "\\*gud-" (buffer-name buf))
+                 (throw 'found buf))))))
+    (if gud-buf
+        (switch-to-buffer-other-window gud-buf)
+      (call-interactively 'gdb))))
+;; --8<-------------------------- separator ------------------------>8--
+(eval-after-load "gdb-ui"
+  '(defun gdb-display-buffer (buf dedicated &optional frame)
+     (let ((answer (get-buffer-window buf (or frame 0))))
+       (if answer
+           (display-buffer buf t (or frame 0)) ;Deiconify the frame if necessary.
+         (let ((window (get-lru-window)))
+           (if (memq (buffer-local-value 'gud-minor-mode (window-buffer window))
+                     '(gdba gdbmi))
+               (let* ((largest (get-largest-window))
+                      (cur-size (window-height largest)))
+                 (setq answer (split-window largest))
+                 (set-window-buffer answer buf)
+                 (set-window-dedicated-p answer dedicated)
+                 answer)
+             (set-window-buffer window buf)
+             window))))))
+
+;;--------------------enable webjump in c++ mode
+(define-key c-mode-base-map [(meta j)] 'webjump)
