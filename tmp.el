47156515154d3a45fc576a52b58d267f6f145034
ning <ning@laptop.(none)>
2013-08-26 00:18:19 -0400

    add ergoemacs-mode

--- tmp.el
+++ tmp.el
@@ -5,6 +5,13 @@
 ;; Description :
 ;; --
 ;; Created : <2013-05-11>
-;; Updated: Time-stamp: <2013-05-11 09:12:09>
+;; Updated: Time-stamp: <2013-08-26 00:07:55>
 ;;-------------------------------------------------------------------
 ;; File : tmp.el ends
+
+;; try using ergoemac mode by 
+(add-to-list 'load-path (concat EMACS_VENDOR "/ergoemacs-mode"))
+(require 'ergoemacs-mode)
+(setq ergoemacs-theme "lvl3")
+(setq ergoemacs-keyboard-layout "us")
+(ergoemacs-mode 1)
