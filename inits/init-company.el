2876045e7ddb3f9217a64c253fba4291189c1435
Ning Zhou <ningzhou.cn@gmail.com>
2016-06-12 23:02:19 -0700

    company mode

--- inits/init-company.el
+++ inits/init-company.el
@@ -1,22 +1,6 @@
-;;-------------------------------------------------------------------
-;; Copyright (C) 2013 Ning Zhou
-;; File : init-company.el
-;; Author : Ning Zhou <nzhoun@gmail.com>
-;; Description :
-;; --
-;; Created : <2015-03-08>
-;; Updated: Time-stamp: <2015-06-28 22:22:04>
-;;-------------------------------------------------------------------
+(add-to-list 'load-path (concat EMACS_VENDOR "/company-statistics"))
 
-(add-hook 'prog-mode-hook 'global-company-mode)
-(add-hook 'cmake-mode-hook 'global-company-mode)
-
-;; does not matter, I never use this hotkey
-(global-set-key (kbd "C-c o") 'company-complete)
-(setq company-require-match nil)
-;; press SPACE will accept the highlighted candidate and insert a space
-;; `M-x describe-variable company-auto-complete-chars` for details
-(setq company-auto-complete t)
+(add-hook 'after-init-hook 'global-company-mode)
 
 (if (fboundp 'evil-declare-change-repeat)
     (mapc #'evil-declare-change-repeat
@@ -29,19 +13,77 @@
 
 (eval-after-load 'company
   '(progn
+     ;; @see https://github.com/company-mode/company-mode/issues/348
+     (unless (featurep 'company-statistics)
+       (require 'company-statistics))
+     (company-statistics-mode)
+
      (add-to-list 'company-backends 'company-cmake)
      (add-to-list 'company-backends 'company-c-headers)
      ;; can't work with TRAMP
      (setq company-backends (delete 'company-ropemacs company-backends))
      ;; (setq company-backends (delete 'company-capf company-backends))
+
      ;; I don't like the downcase word in company-dabbrev
      ;; for languages use camel case naming convention
+     ;; company should be case sensitive
      (setq company-dabbrev-downcase nil)
+     (setq company-dabbrev-ignore-case nil)
      (setq company-show-numbers t)
-     (setq company-begin-commands '(self-insert-command))
      (setq company-idle-delay 0.2)
      (setq company-clang-insert-arguments nil)
-     ))
+     (setq company-require-match nil)
+     (setq company-etags-ignore-case t)
+
+     ;; @see https://github.com/redguardtoo/emacs.d/commit/2ff305c1ddd7faff6dc9fa0869e39f1e9ed1182d
+     (defadvice company-in-string-or-comment (around company-in-string-or-comment-hack activate)
+       ;; you can use (ad-get-arg 0) and (ad-set-arg 0) to tweak the arguments
+       (if (memq major-mode '(php-mode html-mode web-mode nxml-mode))
+           (setq ad-return-value nil)
+         ad-do-it))
+
+     ;; press SPACE will accept the highlighted candidate and insert a space
+     ;; `M-x describe-variable company-auto-complete-chars` for details
+     ;; That's BAD idea.
+     (setq company-auto-complete nil)
+
+     ;; NOT to load company-mode for certain major modes.
+     ;; Ironic that I suggested this feature but I totally forgot it
+     ;; until two years later.
+     ;; https://github.com/company-mode/company-mode/issues/29
+     (setq company-global-modes
+           '(not
+             eshell-mode comint-mode erc-mode gud-mode rcirc-mode
+             minibuffer-inactive-mode))))
+
+;; {{ setup company-ispell
+(defun toggle-company-ispell ()
+  (interactive)
+  (cond
+   ((memq 'company-ispell company-backends)
+    (setq company-backends (delete 'company-ispell company-backends))
+    (message "company-ispell disabled"))
+   (t
+    (add-to-list 'company-backends 'company-ispell)
+    (message "company-ispell enabled!"))))
+
+(defun company-ispell-setup ()
+  ;; @see https://github.com/company-mode/company-mode/issues/50
+  (when (boundp 'company-backends)
+    (make-local-variable 'company-backends)
+    (add-to-list 'company-backends 'company-ispell)
+    (setq company-ispell-dictionary ispell-alternate-dictionary)))
+
+;; message-mode use company-bbdb.
+;; So we should NOT turn on company-ispell
+(add-hook 'org-mode-hook 'company-ispell-setup)
+;; }}
+
+(eval-after-load 'company-etags
+  '(progn
+     ;; insert major-mode not inherited from prog-mode
+     ;; to make company-etags work
+     (add-to-list 'company-etags-modes 'web-mode)
+     (add-to-list 'company-etags-modes 'lua-mode)))
 
 (provide 'init-company)
-;; File : init-company.el ends
